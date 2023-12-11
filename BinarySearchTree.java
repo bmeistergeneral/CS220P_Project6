3248a8d20879846677b5e1d9e297f01a9be4f49c
bmeistergeneral <brecon.morgan@gmail.com>
2023-12-11 18:57:16 -0500

    Included JavaDoc documentation

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -4,43 +4,90 @@ public class BinarySearchTree <T extends Comparable<T>> {
     private static class Node {
         private Node left, right;
         private int data;
+
+        /**
+         *
+         * @param data variable
+         */
         public Node(int data) {
             left = null;
             right = null;
             this.data = data;
         }
 
+        /**
+         *
+         * @return left
+         */
         public Node getLeft() {
             return left;
         }
+
+        /**
+         *
+         * @return right
+         */
         public Node getRight() {
             return right;
         }
 
+        /**
+         *
+         * @return data
+         */
         public int getData() {
             return data;
         }
 
+        /**
+         *
+         * @param left node
+         */
         public void setLeft(Node left) {
             this.left = left;
         }
 
+        /**
+         *
+         * @param right node
+         */
         public void setRight(Node right) {
             this.right = right;
         }
+
+        /**
+         *
+         * @param data variable
+         */
         public void setData(int data) {
             this.data = data;
         }
     }
     private Node root;
+
+    /**
+     * constructor
+     */
     public BinarySearchTree() {
         root = null;
     }
+
+    /**
+     *
+     * @param root getHeight method returns the height from the root node down to bottom node
+     * @return height
+     */
    public int getHeight(Node root) {
         return height(root);
    }
 
    // prints height of the tree from the root down to bottom leaf node.
+
+    /**
+     * height method to calculate height
+     * @param n height method to calculate height
+     * @return left and right subtree depths
+     */
    private int height(Node n) {
         if (n == null) {
             return 0;
@@ -56,6 +103,11 @@ public class BinarySearchTree <T extends Comparable<T>> {
     }
 
     // prints the data in preorder traversal from root n.
+
+    /**
+     *
+     * @param n root node of print method
+     */
    private void preFixPrint(Node n) {
         if (n == null) {
             return;
@@ -65,6 +117,12 @@ public class BinarySearchTree <T extends Comparable<T>> {
         preFixPrint(n.right);
     }
     // counts the number of leaf nodes in the BST
+
+    /**
+     *
+     * @param n starting from root node and working through each subtree.
+     * @return number of leaf nodes
+     */
     private int countLeafNodes(Node n) {
         if (n == null) {
             return 0;
@@ -76,6 +134,12 @@ public class BinarySearchTree <T extends Comparable<T>> {
         }
     }
     // counts the number of total nodes.
+
+    /**
+     *
+     * @param n each node in the BST
+     * @return calculate total nodes.
+     */
     public int countNodes(Node n) {
         if (n == null) {
             return 0;
@@ -85,10 +149,22 @@ public class BinarySearchTree <T extends Comparable<T>> {
         return 1 + l + r;
     }
     // connector code for insert method below
+
+    /**
+     *
+     * @param value value of each node inserted
+     */
     public void insert(int value) {
        this.root = insert(root, value);
     }
     // inserts each node in the binary search tree including the root
+
+    /**
+     *
+     * @param n node in the BST
+     * @param value its value
+     * @return the root node if it's an empty tree
+     */
    public Node insert(Node n, int value) {
         // if tree is empty create a root node.
     if (n == null) {
@@ -104,6 +180,13 @@ public class BinarySearchTree <T extends Comparable<T>> {
     return n;
    }
    // searches for a specific node in the binary search tree
+
+    /**
+     *
+     * @param n node in BST
+     * @param value its value
+     * @return whether node was found or not
+     */
     private boolean search(Node n, int value) {
     if (n == null) {
         return false;
@@ -118,10 +201,21 @@ public class BinarySearchTree <T extends Comparable<T>> {
     return search(n.getRight(), value);
     }
     // connector code for search method
+
+    /**
+     *
+     * @param data node data to be searched for
+     * @return whether node was found or not
+     */
     public boolean search(int data) {
        return search(this.root, data);
     }
     // Driver code
+
+    /**
+     *
+     * @param args -
+     */
     public static void main(String[] args) {
        BinarySearchTree T = new BinarySearchTree();
         T.insert(3);
