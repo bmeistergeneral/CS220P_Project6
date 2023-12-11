97f5a1c30bba2e8d756d02d60e0e63155e20a88a
bmeistergeneral <brecon.morgan@gmail.com>
2023-12-11 17:42:32 -0500

    Added comments and used different test cases

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -36,12 +36,11 @@ public class BinarySearchTree <T extends Comparable<T>> {
     public BinarySearchTree() {
         root = null;
     }
-    /* public void print(Node n) {
-
-    } */
    public int getHeight(Node root) {
         return height(root);
    }
+
+   // prints height of the tree from the root down to bottom leaf node.
    private int height(Node n) {
         if (n == null) {
             return 0;
@@ -55,9 +54,8 @@ public class BinarySearchTree <T extends Comparable<T>> {
             }
         }
     }
-   /* public void preorder(Node root) {
-       preorder(this.root);
-    } */
+
+    // prints the data in preorder traversal from root n.
    private void preFixPrint(Node n) {
         if (n == null) {
             return;
@@ -66,6 +64,7 @@ public class BinarySearchTree <T extends Comparable<T>> {
         preFixPrint(n.left);
         preFixPrint(n.right);
     }
+    // counts the number of leaf nodes in the BST
     private int countLeafNodes(Node n) {
         if (n == null) {
             return 0;
@@ -76,6 +75,7 @@ public class BinarySearchTree <T extends Comparable<T>> {
             return countLeafNodes(n.left) + countLeafNodes(n.right);
         }
     }
+    // counts the number of total nodes.
     public int countNodes(Node n) {
         if (n == null) {
             return 0;
@@ -84,51 +84,62 @@ public class BinarySearchTree <T extends Comparable<T>> {
         int r = countNodes(n.right);
         return 1 + l + r;
     }
+    // connector code for insert method below
     public void insert(int value) {
        this.root = insert(root, value);
     }
+    // inserts each node in the binary search tree including the root
    public Node insert(Node n, int value) {
+        // if tree is empty create a root node.
     if (n == null) {
         n = new Node(value);
         return n;
+        // if the root data is greater than element data insert at left child
     } else if (n.data >= value) {
-        n.left = insert(n.left, value);
+        n.setLeft(insert(n.left, value));
+        // else insert ar right child.
     } else {
-        n.right = insert(n.right, value);
+        n.setRight(insert(n.right, value));
     }
     return n;
    }
+   // searches for a specific node in the binary search tree
     private boolean search(Node n, int value) {
     if (n == null) {
         return false;
+        // if node data is equal to the search value, it's found
     } else if (n.getData() == value) {
         return true;
+        // if it's greater than the search value, search left subtree of that node
     } else if (n.getData() > value) {
-        return search(n.left, value);
+        return search(n.getLeft(), value);
     }
-    return search(n.right, value);
+    // else search the right subtree of that node
+    return search(n.getRight(), value);
     }
+    // connector code for search method
     public boolean search(int data) {
        return search(this.root, data);
     }
+    // Driver code
     public static void main(String[] args) {
        BinarySearchTree T = new BinarySearchTree();
-        T.insert(3);
+        T.insert(4);
         T.preFixPrint(T.root);
         T.insert(1);
         T.preFixPrint(T.root.left);
-        T.insert(4);
+        T.insert(5);
         T.preFixPrint(T.root.right);
-        T.insert(6);
+        T.insert(7);
         T.preFixPrint(T.root.right.right);
-        T.insert(9);
+        T.insert(8);
         T.preFixPrint(T.root.right.right.right);
         T.insert(2);
         T.preFixPrint(T.root.left.right);
-        T.insert(5);
+        T.insert(6);
         T.preFixPrint(T.root.right.right.left);
-        T.insert(7);
-        T.preFixPrint(T.root.right.right.right.left);
+        T.insert(9);
+        T.preFixPrint(T.root.right.right.right.right);
         System.out.println();
         System.out.println("Leaf Nodes: " + T.countLeafNodes(T.root));
         System.out.println("Total Nodes: " + T.countNodes(T.root));
